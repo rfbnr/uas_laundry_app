93cc57f09a419e7a0082e77d2ad742493007e9b7
rfbnr <ridwanfebnur9@gmail.com>
2024-12-25 01:04:34 +0700

    root app: fix initial home

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
-import 'src/features/home/screens/home_screen.dart';
+import 'src/data/datasources/auth_local_datasource.dart';
+import 'src/features/auth/bloc/auth_bloc.dart';
+import 'src/features/auth/screens/login_screen.dart';
+import 'src/features/home/screens/dashboard_route.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,25 +25,38 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Laundry App',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          surfaceTintColor: Colors.white,
-          systemOverlayStyle: SystemUiOverlayStyle(
-            statusBarColor: Colors.transparent,
-            statusBarIconBrightness: Brightness.dark,
+    return BlocProvider(
+      create: (context) => AuthBloc(
+        authLocalDatasource: AuthLocalDatasource(),
+      )..add(UserIsLogin()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Laundry App',
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            surfaceTintColor: Colors.white,
+            systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarColor: Colors.transparent,
+              statusBarIconBrightness: Brightness.dark,
+            ),
           ),
         ),
+        builder: EasyLoading.init(),
+        home: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state.status == AuthStatus.success) {
+              return const DashboardRoute();
+            } else {
+              return const LoginScreen();
+            }
+          },
+        ),
       ),
-      builder: EasyLoading.init(),
-      home: const HomeScreen(),
     );
   }
 }
