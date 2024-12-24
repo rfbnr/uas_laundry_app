ac5ca87bfc41ab555fc9aa31df36b7836fd826ce
rfbnr <ridwanfebnur9@gmail.com>
2024-12-25 01:06:11 +0700

    feat(profile): add profile and logout

---
+++ lib/src/features/profile/widgets/detail_data_profile_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class DetailDataProfileWidget extends StatelessWidget {
+  const DetailDataProfileWidget({
+    super.key,
+    required this.title,
+    required this.value,
+  });
+
+  final String title;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
+    );
+  }
+}
