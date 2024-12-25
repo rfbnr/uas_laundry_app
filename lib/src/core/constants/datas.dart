ed7fc0415e2503feead4897f1f01be2aa654b0e7
rfbnr <ridwanfebnur9@gmail.com>
2024-12-25 23:42:47 +0700

    style(data constants): add washing type data and fragrance type data

---
+++ lib/src/core/constants/datas.dart
@@ -0,0 +1,37 @@
+final List<Map<String, dynamic>> fragranceTypeData = [
+  {
+    'pewangiId': 1,
+    'pewangiName': "Lavender",
+  },
+  {
+    'pewangiId': 2,
+    'pewangiName': "Jasmine",
+  },
+  {
+    'pewangiId': 3,
+    'pewangiName': "Vanilla",
+  },
+  {
+    'pewangiId': 4,
+    'pewangiName': "Rose",
+  },
+  {
+    'pewangiId': 5,
+    'pewangiName': "Lemon",
+  },
+];
+
+final List<Map<String, dynamic>> washingTypeData = [
+  {
+    'cucianId': 1,
+    'cucianName': "Cuci Kering",
+  },
+  {
+    'cucianId': 2,
+    'cucianName': "Cuci Basah",
+  },
+  {
+    'cucianId': 3,
+    'cucianName': "Cuci Setrika",
+  },
+];
